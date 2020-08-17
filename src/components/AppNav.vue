a9e1c9c69af008b0194607379c1c0a371a764956
Raul Novelo <rnovelo.cruz98@gmail.com>
2020-08-16 22:01:10 -0500

    Iconos

--- src/components/AppNav.vue
+++ src/components/AppNav.vue
@@ -3,7 +3,11 @@
     <div class="navbar-brand">
       <div class="navbar-item">
         <div class="control has-icons-left">
-          <input class="input is-small has-text-centered" type="text" placeholder="search" />
+          <input
+            class="input is-small has-text-centered"
+            type="text"
+            placeholder="search"
+          />
           <span class="icon is-left is-small">
             <i class="material-icons">search</i>
           </span>
@@ -21,63 +25,65 @@
         <a class="navbar-item">
           <i class="material-icons">notifications</i>
         </a>
-        <b-dropdown                    
-                    v-model="navigation"
-                    position="is-bottom-left"
-                    append-to-body
-                    aria-role="menu">
-                    <a
-                        class="navbar-item"
-                        slot="trigger"
-                        role="button">
-                        <i class="material-icons">person_outline</i>
-                        <b-icon icon="menu-down"></b-icon>
-                    </a>
-                    <b-dropdown-item custom aria-role="menuitem">
-                        Registrado como <b>Jacinto Canek</b>
-                    </b-dropdown-item>
-                    <hr class="dropdown-divider">
-                    <b-dropdown-item value="home" aria-role="menuitem">
-                        <b-icon icon="home"></b-icon>
-                        <router-link tag="span" to="/home">
-                          Inicio
-                        </router-link>
-                    </b-dropdown-item>
-                    <b-dropdown-item value="blog" aria-role="menuitem">
-                        <b-icon icon="book-open"></b-icon>
-                        <router-link tag="span" to="/about">
-                          Mis solicitudes
-                        </router-link>
-                    </b-dropdown-item>
-                    <hr class="dropdown-divider" aria-role="menuitem">
-                    <b-dropdown-item value="settings">
-                        <b-icon icon="settings"></b-icon>
-                        Configuración
-                    </b-dropdown-item>
-                    <b-dropdown-item value="logout" aria-role="menuitem">
-                        <b-icon icon="logout"></b-icon>
-                        Cerrar Sesión
-                    </b-dropdown-item>
-                </b-dropdown>
+        <b-dropdown
+          v-model="navigation"
+          position="is-bottom-left"
+          append-to-body
+          aria-role="menu"
+        >
+          <a class="navbar-item" slot="trigger" role="button">
+            <i class="material-icons">person_outline</i>
+            <b-icon icon="menu-down"></b-icon>
+          </a>
+          <b-dropdown-item custom aria-role="menuitem">
+            Registrado como <b>Jacinto Canek</b>
+          </b-dropdown-item>
+          <hr class="dropdown-divider" />
+          <b-dropdown-item has-link value="home" aria-role="menuitem">
+            <router-link tag="a" to="/home">
+              <b-icon icon="home"></b-icon>
+              Inicio
+            </router-link>
+          </b-dropdown-item>
+          <b-dropdown-item has-link value="blog" aria-role="menuitem">
+            <router-link tag="a" to="/about">
+              <b-icon icon="book-open"></b-icon>
+              Mis solicitudes
+            </router-link>
+          </b-dropdown-item>
+          <hr class="dropdown-divider" aria-role="menuitem" />
+          <b-dropdown-item has-link value="settings">
+            <router-link tag="a" to="/about">
+              <b-icon icon="cog"></b-icon>
+              Configuración
+            </router-link>
+          </b-dropdown-item>
+          <b-dropdown-item value="logout" aria-role="menuitem">
+            <b-icon icon="logout"></b-icon>
+            Cerrar Sesión
+          </b-dropdown-item>
+        </b-dropdown>
       </template>
       <div v-else class="buttons">
         <router-link tag="a" to="/register" class="button is-primary">
           <strong>Sign up</strong>
         </router-link>
-        <router-link tag="a" to="/login" class="button is-light">Log in</router-link>
+        <router-link tag="a" to="/login" class="button is-light"
+          >Log in</router-link
+        >
       </div>
     </div>
   </div>
 </template>
 
 <script>
-    export default {
-        data() {
-            return {
-                navigation: 'home'
-            }
-        }
+export default {
+  data () {
+    return {
+      navigation: 'home'
     }
+  }
+}
 </script>
 
 <style scoped>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,7 +2,7 @@
   <section class="hero is-success is-fullheight">
     <div class="hero-body">
       <div class="container has-text-centered">
-        <div class="column is-5 is-offset-3">
+        <div class="column is-4 is-offset-4">
           <div class="box">
             <h1>Ventanilla Digital</h1>
             <figure class="avatar">
