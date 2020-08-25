a1b60a6aa37363ad77d25f704b820a7c12f771fa
Raul Novelo <rnovelo.cruz98@gmail.com>
2020-08-24 21:43:47 -0500

    PWA

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,37 +1,37 @@
 workbox.setConfig({
-    debug: false
+  debug: false
+})
+
+workbox.precaching.precacheAndRoute([])
+
+workbox.routing.registerRoute(
+  /\.(?:png|gif|jpg|jpeg|svg)$/,
+  workbox.strategies.staleWhileRevalidate({
+    cacheName: 'images',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 60,
+        maxAgeSeconds: 30 * 24 * 60 * 60 // 30 Days
+      })
+    ]
   })
-  
-  workbox.precaching.precacheAndRoute([])
-  
-  workbox.routing.registerRoute(
-    /\.(?:png|gif|jpg|jpeg|svg)$/,
-    workbox.strategies.staleWhileRevalidate({
-      cacheName: 'images',
-      plugins: [
-        new workbox.expiration.Plugin({
-          maxEntries: 60,
-          maxAgeSeconds: 30 * 24 * 60 * 60 // 30 Days
-        })
-      ]
-    })
-  )
-  
-  workbox.routing.registerRoute(
-    new RegExp('https://digital-window.herokuapp.com'),
-    workbox.strategies.networkFirst({
-      cacheName: 'api'
-    })
-  )
-  
-  workbox.routing.registerRoute(
-    new RegExp('https://fonts.(?:googleapis|gstatic).com/(.*)'),
-    workbox.strategies.cacheFirst({
-      cacheName: 'googleapis',
-      plugins: [
-        new workbox.expiration.Plugin({
-          maxEntries: 30
-        })
-      ]
-    })
-  )
+)
+
+workbox.routing.registerRoute(
+  new RegExp('https://digital-window.herokuapp.com'),
+  workbox.strategies.networkFirst({
+    cacheName: 'api'
+  })
+)
+
+workbox.routing.registerRoute(
+  new RegExp('https://fonts.(?:googleapis|gstatic).com/(.*)'),
+  workbox.strategies.cacheFirst({
+    cacheName: 'googleapis',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 30
+      })
+    ]
+  })
+)
