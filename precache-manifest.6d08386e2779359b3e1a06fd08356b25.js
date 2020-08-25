fe2d313aff27d710e7634782e657c55e83d21f2f
Ivan Mota <ivan_mota09@hotmail.com>
2020-08-24 23:13:23 -0500

    Updates

---
+++ precache-manifest.6d08386e2779359b3e1a06fd08356b25.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3566755cafe33e16abd",
+    "url": "/digital-window/css/app.db3a9583.css"
+  },
+  {
+    "revision": "f16dc660ac8ac3ca944a",
+    "url": "/digital-window/css/chunk-096008d0.ebac6dfb.css"
+  },
+  {
+    "revision": "fcf320220dff48137a0f",
+    "url": "/digital-window/css/chunk-25719759.c8116691.css"
+  },
+  {
+    "revision": "08f6d2f7795b60d12886",
+    "url": "/digital-window/css/chunk-551ee649.9ed3df40.css"
+  },
+  {
+    "revision": "f662880fd84e4e7d7cc7",
+    "url": "/digital-window/css/chunk-7e258894.bbb19a0d.css"
+  },
+  {
+    "revision": "f3f13d6df1c846f66a93",
+    "url": "/digital-window/css/chunk-f9af96f8.cd3057b7.css"
+  },
+  {
+    "revision": "01479b33427f0a971076",
+    "url": "/digital-window/css/chunk-vendors.4d846261.css"
+  },
+  {
+    "revision": "7eb4ee07d5201b9b161d37c8890beb92",
+    "url": "/digital-window/img/Studying-pana.7eb4ee07.svg"
+  },
+  {
+    "revision": "cd4f0a1da69377b65f518f1f1e459e05",
+    "url": "/digital-window/img/estudiante.cd4f0a1d.jpg"
+  },
+  {
+    "revision": "45a9cc88f1929ffa46c3df0746208254",
+    "url": "/digital-window/img/horario.45a9cc88.jpg"
+  },
+  {
+    "revision": "ae8f45fb9041729e26e9f103267edb44",
+    "url": "/digital-window/img/seguimiento.ae8f45fb.jpg"
+  },
+  {
+    "revision": "72c462e9c5656a38f113af66fe2675e2",
+    "url": "/digital-window/index.html"
+  },
+  {
+    "revision": "b3566755cafe33e16abd",
+    "url": "/digital-window/js/app-legacy.e7be6a2e.js"
+  },
+  {
+    "revision": "f16dc660ac8ac3ca944a",
+    "url": "/digital-window/js/chunk-096008d0-legacy.d2b13976.js"
+  },
+  {
+    "revision": "fcf320220dff48137a0f",
+    "url": "/digital-window/js/chunk-25719759-legacy.7ef2f0a4.js"
+  },
+  {
+    "revision": "7b4df1537a28e159b8bb",
+    "url": "/digital-window/js/chunk-2d0d79be-legacy.6fa5faf6.js"
+  },
+  {
+    "revision": "929f5d6d45493fcacc9f",
+    "url": "/digital-window/js/chunk-2d22d746-legacy.1c4446db.js"
+  },
+  {
+    "revision": "08f6d2f7795b60d12886",
+    "url": "/digital-window/js/chunk-551ee649-legacy.0eeea21c.js"
+  },
+  {
+    "revision": "f662880fd84e4e7d7cc7",
+    "url": "/digital-window/js/chunk-7e258894-legacy.71e65e5d.js"
+  },
+  {
+    "revision": "f3f13d6df1c846f66a93",
+    "url": "/digital-window/js/chunk-f9af96f8-legacy.66f3216d.js"
+  },
+  {
+    "revision": "01479b33427f0a971076",
+    "url": "/digital-window/js/chunk-vendors-legacy.2327e010.js"
+  },
+  {
+    "revision": "63d3d6b27793c7a831e37164f06851a7",
+    "url": "/digital-window/manifest.json"
+  }
+]);
